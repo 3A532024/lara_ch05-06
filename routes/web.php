4d052928d32edc30f77a771147ee9437dc2dd925
3A532024 <kc1223127@gmail.com>
2018-11-05 09:25:37 +0800

    練習2：基本路由參數(複數個路由參數)

--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,4 @@ Route::get('student/{student_no}', function ($student_no) {
     return "學號:".$student_no;
 });
 Route::get('student/{student_no}/score/{subject?}',function($student_no,$subject) {
-    return "學號:".$student_no."的所有成績";
-});
+    return "學號:".$student_no."的".$subject."成績";});
