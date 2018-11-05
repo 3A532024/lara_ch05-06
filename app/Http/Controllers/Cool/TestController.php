c60ab6231a1f78bf9c472fb27603cbdc80435b09
3A532024 <kc1223127@gmail.com>
2018-11-05 10:01:06 +0800

    練習10-1：產生Cool\TestController,新增路由'cool'

---
+++ app/Http/Controllers/Cool/TestController.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Http\Controllers\Cool;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+class TestController extends Controller
+{
+    public function index(){
+        return 'Cooler';
+    }
+}
